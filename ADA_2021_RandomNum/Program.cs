c0f6b432ff4535fc15eb08be3d76eae39d33b780
Sepu2002 <sslpanteras@gmail.com>
2021-08-20 08:28:00 -0500

    randum num added

---
+++ ADA_2021_RandomNum/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace ADA_2021_RandomNum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int randnum;
+
+            Random genAleatorio = new Random();
+
+            randnum = genAleatorio.Next(1, 7);
+
+            Console.WriteLine("Número Aleatorio: " + randnum);
+
+            Console.WriteLine("               .........");
+            Console.WriteLine("              :~, *   * ~,");
+            Console.WriteLine("              : ~, *   * ~.");
+            Console.WriteLine("              :  ~........~");
+            Console.WriteLine("              : *:         :      ~'~,");
+            Console.WriteLine("              :  :         :    ~' *  ~,");
+            Console.WriteLine("              ~* :    *    : ,~' *    * ~,");
+            Console.WriteLine("               ~,:         :.~,*    *  ,~ :");
+            Console.WriteLine("                ~:.........::  ~, *  ,~   :");
+            Console.WriteLine("                            : *  ~,,~  *  :");
+            Console.WriteLine("                            :* * * :  *   :");
+            Console.WriteLine("                             ~, *  : *  ,~");
+            Console.WriteLine("                               ~,  :  ,~");
+            Console.WriteLine("                                 ~,:,~");
+           
+
+            Console.WriteLine("Programa exitoso, oprima <enter> ");
+            string espera = Console.ReadLine();
+        }
+    }
+}
