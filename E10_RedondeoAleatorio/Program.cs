164fde0bd6e6b4a63bbc04c6579644fa5b6e8dff
Sepu2002 <sslpanteras@gmail.com>
2021-08-22 15:40:59 -0500

    e10 with random numbers

---
+++ E10_RedondeoAleatorio/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace E10_RedondeoAleatorio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num, ant, post;
+
+            Random genRand = new Random();
+
+
+            Console.WriteLine("Programa que Redondea un numero a las centenas más cercanas ");
+
+            Console.Write("Ingresa el  numero entero:  ");
+            num = Convert.ToInt32(genRand.Next(0, 9999)); ;
+            Console.WriteLine("Numero : " + num);
+
+            ant = num - (num % 100);
+            post = num - num % 100 + 100;
+
+            Console.WriteLine("Centena Anterior  = " + ant);
+            Console.WriteLine("Centena Siguiente = " + post);
+
+            Console.WriteLine("                    *****************");
+            Console.WriteLine("               ******               ******");
+            Console.WriteLine("           ****                           ****");
+            Console.WriteLine("        ****                                 ***");
+            Console.WriteLine("      ***                                       ***");
+            Console.WriteLine("     **           ***               ***           **");
+            Console.WriteLine("   **           *******           *******          ***");
+            Console.WriteLine("  **            *******           *******            **");
+            Console.WriteLine(" **             *******           *******             **");
+            Console.WriteLine(" **               ***               ***               **");
+            Console.WriteLine("**                                                     **");
+            Console.WriteLine("**       *                                     *       **");
+            Console.WriteLine("**      **                                     **      **");
+            Console.WriteLine(" **   ****                                     ****   **");
+            Console.WriteLine(" **      **                                   **      **");
+            Console.WriteLine(" **       ***                             ***       **");
+            Console.WriteLine("  ***       ****                       ****       ***");
+            Console.WriteLine("    **         ******             ******         **");
+            Console.WriteLine("     ***            ***************            ***");
+            Console.WriteLine("        ****                                 ****");
+            Console.WriteLine("           ****                           ****");
+            Console.WriteLine("               ******               ******");
+            Console.WriteLine("                    *****************");
+
+            Console.WriteLine("Programa exitoso, oprima <enter> ");
+            string espera = Console.ReadLine();
+        }
+    }
+}
