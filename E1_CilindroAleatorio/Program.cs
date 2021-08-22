6017385cee90b88459ae618c794ade555441b0d9
Sepu2002 <sslpanteras@gmail.com>
2021-08-22 15:27:46 -0500

    variant of cylinder calculator

--- E1_CilindroAleatorio/Program.cs
+++ E1_CilindroAleatorio/Program.cs
@@ -1,32 +1,35 @@
 ﻿using System;
 
-namespace E1_Cilindro
+namespace E1_CilindroAleatorio
 {
-	class Program
-	{
-		static void Main(string[] args)
-		{
-			Console.WriteLine("Programa que calcula el volumen y área lateral de un cilindro");
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Programa que calcula el volumen y área lateral de un cilindro con valores aleatorios");
 
-			double rad, height, vol, area;
+            double rad, height, vol, area;
 
-			Console.WriteLine("Ingresa el radio: ");
-			rad = Convert.ToDouble(Console.ReadLine());
-			Console.WriteLine("Radio: " + rad);
+            Random randRad = new Random();
+            Random randHei = new Random();
 
-			Console.WriteLine("Ingresa la altura: ");
-			height = Convert.ToDouble(Console.ReadLine());
-			Console.WriteLine("Altura: " + height);
+            Console.WriteLine("Radio generado: ");
+            rad = Convert.ToDouble(randRad.Next(1, 5));
+            Console.WriteLine("Radio: " + rad);
 
-			vol = Math.PI * rad * rad * height;
+            Console.WriteLine("Altura generada: ");
+            height = Convert.ToDouble(randHei.Next(1, 20));
+            Console.WriteLine("Altura: " + height);
 
-			area = 2 * rad * Math.PI * height;
+            vol = Math.PI * rad * rad * height;
+
+            area = 2 * rad * Math.PI * height;
 
 
-			Console.WriteLine("El volumen del cilindro es: " + vol);
-			Console.WriteLine("El área lateral del cilindro es: " + area);
+            Console.WriteLine("El volumen del cilindro es: " + vol);
+            Console.WriteLine("El área lateral del cilindro es: " + area);
 
-			Console.WriteLine("");
+            Console.WriteLine("");
 
             Console.WriteLine("                    .------.");
             Console.WriteLine("                   (        )");
@@ -46,8 +49,10 @@ namespace E1_Cilindro
             Console.WriteLine(" .-~                ~-..-         -.   |");
             Console.WriteLine(" |.                  .||-_       _-|   |");
 
+          
+
             Console.WriteLine("Programa exitoso, oprima <enter> ");
             string espera = Console.ReadLine();
         }
-	}
+    }
 }
