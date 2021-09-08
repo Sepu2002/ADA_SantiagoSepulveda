48772dc333cc857617b187e60dd7de6fe3c8f004
Sepu2002 <sslpanteras@gmail.com>
2021-09-08 08:50:57 -0500

    new thingy

---
+++ E_AdivinaNum/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace E_AdivinaNum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Juego para que usuario adivine un número del 0 al 999");
+        }
+    }
+}
---
+++ E_Factorial/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace E_Factorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, i ;
+            long fact, sum;
+            fact = 1;
+            sum = 0;
+            Console.WriteLine("Programa que obtiene el factorial de un número");
+            Console.WriteLine("Escribe un número:");
+            n = Convert.ToInt32(Console.ReadLine());
+
+            if (n == 0)
+            {
+                Console.WriteLine("| 0 Factorial es 1 |");
+                Console.WriteLine("| La sumatoria es 0|");
+            }
+            else {
+                for (i = 1; i <= n; i++)
+                {
+                    fact = fact * i;
+                    sum = sum + i;
+                }
+                Console.WriteLine("| {0} Factorial es: {1} |", n, fact);
+                Console.WriteLine("| La sumatoria de 0 hasta {0} es {1} |", n, sum);
+            }
+
+            Console.WriteLine("           ____");
+            Console.WriteLine("         o8%8888,");
+            Console.WriteLine("       o88%8888888.");
+            Console.WriteLine("      8'-    -:8888b ");
+            Console.WriteLine("     8'         8888");
+            Console.WriteLine("    d8.-=. ,==-.:888b");
+            Console.WriteLine("    >8 `~` :`~' d8888");
+            Console.WriteLine("    88         ,88888");
+            Console.WriteLine("    88b. `-~  ':88888");
+            Console.WriteLine("    888b ~==~ .:88888 ");
+            Console.WriteLine("    88888o--:':::8888");
+            Console.WriteLine("    `88888| :::' 8888b");
+            Console.WriteLine("    8888^^'       8888b");
+            Console.WriteLine("   d888           ,%888b.");
+            Console.WriteLine("  d88%            %%%8--'-.");
+            Console.WriteLine(" /88:.__ ,       _%-' ---  -");
+            Console.WriteLine("     '''::===..-'   =  --.  `");
+            Console.WriteLine("    Santiago Sepúlveda");
+
+            Console.WriteLine("Programa exitoso, oprima <enter> ");
+            string espera = Console.ReadLine();
+        }
+    }
+}
