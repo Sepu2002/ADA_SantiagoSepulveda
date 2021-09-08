89b31543346c7401b10cac2bc594c22b90eedc61
Sepu2002 <sslpanteras@gmail.com>
2021-09-08 16:59:26 -0500

    color

--- E_AdivinaNum/Program.cs
+++ E_AdivinaNum/Program.cs
@@ -18,10 +18,12 @@ namespace E_AdivinaNum
                 Console.WriteLine("Juego para que usuario adivine un número del 0 al 999");
                 Console.WriteLine("Escriba el número que crea que es");
                 inp = Convert.ToInt32(Console.ReadLine());
-                if(inp > randnum)
+                if (inp > randnum)
                 {
+                    Console.ForegroundColor = ConsoleColor.Red;
                     Console.WriteLine("Es menos que eso");
                     tri = tri + 1;
+                    Console.ForegroundColor = ConsoleColor.Yellow;
                     Console.WriteLine("   ;;;;;");
                     Console.WriteLine("   ;;;;;");
                     Console.WriteLine("   ;;;;;");
@@ -30,13 +32,16 @@ namespace E_AdivinaNum
                     Console.WriteLine(" ..;;;;;..");
                     Console.WriteLine("  ':::::'");
                     Console.WriteLine("    ':`");
+                    Console.ForegroundColor = ConsoleColor.White;
                     Console.WriteLine("oprima <enter> ");
                     string espera = Console.ReadLine();
                     Console.Clear();
                 }
                 if (inp < randnum) {
+                    Console.ForegroundColor = ConsoleColor.Red;
                     Console.WriteLine("Es más que eso");
                     tri = tri + 1;
+                    Console.ForegroundColor = ConsoleColor.Yellow;
                     Console.WriteLine("     .");
                     Console.WriteLine("   .:;:.");
                     Console.WriteLine(" .:;;;;;:.");
@@ -45,11 +50,13 @@ namespace E_AdivinaNum
                     Console.WriteLine("   ;;;;;");
                     Console.WriteLine("   ;;;;;");
                     Console.WriteLine("   ;;;;;");
+                    Console.ForegroundColor = ConsoleColor.White;
                     Console.WriteLine("oprima <enter> ");
                     string espera = Console.ReadLine();
                     Console.Clear();
                 }
                 if (inp == randnum) {
+                    Console.ForegroundColor = ConsoleColor.Green;
                     Console.WriteLine("¡Eso es correcto!");
                     Console.WriteLine("Lo lograste en {0} intentos", tri);
                     Console.WriteLine("     _______________");
@@ -63,11 +70,12 @@ namespace E_AdivinaNum
                     Console.WriteLine("        .-'''''-.");
                     Console.WriteLine("      .'  * * *  `.");
                     Console.WriteLine("     :  *       *  :");
-                    Console.WriteLine("    : ~  G O O D  ~ :");
-                    Console.WriteLine("    : ~   J O B   ~ :");
+                    Console.WriteLine("    : ~  G R A N  ~ :");
+                    Console.WriteLine("    : ~  TRABAJO  ~ :");
                     Console.WriteLine("     :  *       *  :");
                     Console.WriteLine("      `.  * * *  .'");
                     Console.WriteLine("        `-.....-'");
+                    Console.ForegroundColor = ConsoleColor.White;
                     Console.WriteLine("oprima <enter> ");
                     string espera = Console.ReadLine();
                     break;
