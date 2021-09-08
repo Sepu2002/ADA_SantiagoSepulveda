53535e548548606231585ab3c747f35fed53558c
Sepu2002 <sslpanteras@gmail.com>
2021-09-08 12:26:23 -0500

    guessing game

--- E_AdivinaNum/Program.cs
+++ E_AdivinaNum/Program.cs
@@ -6,8 +6,41 @@ namespace E_AdivinaNum
     {
         static void Main(string[] args)
         {
+            int tri, randnum, inp;
+            inp = 0;
+            tri = 0;
+            Random genAleatorio = new Random();
+            randnum = genAleatorio.Next(1, 999);
             
-            Console.WriteLine("Juego para que usuario adivine un número del 0 al 999");
+
+            while (true)
+            {
+                Console.WriteLine("Juego para que usuario adivine un número del 0 al 999");
+                Console.WriteLine("Escriba el número que crea que es");
+                inp = Convert.ToInt32(Console.ReadLine());
+                if(inp > randnum)
+                {
+                    Console.WriteLine("Es menos que eso");
+                    tri = tri + 1;
+                    Console.WriteLine("oprima <enter> ");
+                    string espera = Console.ReadLine();
+                    Console.Clear();
+                }
+                if (inp < randnum) {
+                    Console.WriteLine("Es más que eso");
+                    tri = tri + 1;
+                    Console.WriteLine("oprima <enter> ");
+                    string espera = Console.ReadLine();
+                    Console.Clear();
+                }
+                if (inp == randnum) {
+                    Console.WriteLine("¡Eso es correcto!");
+                    Console.WriteLine("Lo lograste en {0} intentos", tri);
+                    Console.WriteLine("oprima <enter> ");
+                    string espera = Console.ReadLine();
+                    break;
+                }
+            }
         }
     }
-}
+}//<>
