b1880210d3b9d19a5ba453e4b7ba45082e170052
Sepu2002 <sslpanteras@gmail.com>
2021-09-22 08:39:40 -0500

    smile game

---
+++ E_SmileXY/Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace E_SmileXY
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Leer teclas
+            ConsoleKeyInfo tecla;
+            char smile = '☻';
+            int max_ancho = 40; int max_alto = 17;
+            Console.SetCursorPosition(1, 1);
+            Console.Write("Smile");
+            int x = 2;
+            int y = 2;
+            Console.SetCursorPosition(x, y);
+            
+            Console.Write(smile);
+
+            do
+            {
+                tecla = Console.ReadKey(false);
+                switch (tecla.Key)
+                {
+                    case ConsoleKey.RightArrow:
+                        if (x<=max_ancho)
+                        {
+                            x++;
+                            Console.Clear();
+                            Console.SetCursorPosition(x, y);
+                            Console.Write(smile);
+                            Console.SetCursorPosition(102, 27);
+                            Console.Write(x + "," + y);
+                            Console.SetCursorPosition(102, 5);
+                            Console.Write("Derecha →");
+                            Console.SetCursorPosition(x, y);
+                        }
+                        break;
+
+
+
+                    case ConsoleKey.LeftArrow:
+                        if (x > 1)
+                        {
+                            x--;
+                            Console.Clear();
+                            Console.SetCursorPosition(x, y);
+                            Console.Write(smile);
+                            Console.SetCursorPosition(102, 27);
+                            Console.Write(x + "," + y);
+                            Console.SetCursorPosition(102, 5);
+                            Console.Write("Izquierda ←");
+                            Console.SetCursorPosition(x, y);
+
+                        }
+                        break;
+                       
+                        
+                    case ConsoleKey.DownArrow:
+                        if (y <= max_alto)
+                        {
+                            y++;
+                            Console.Clear();
+                            Console.SetCursorPosition(x, y);
+                            Console.Write(smile);
+                            Console.SetCursorPosition(102, 27);
+                            Console.Write(x + "," + y);
+                            Console.SetCursorPosition(102, 5);
+                            Console.Write("Abajo ↓");
+                            Console.SetCursorPosition(x, y);
+
+                        }
+                        break;
+                        
+                        
+                    case ConsoleKey.UpArrow:
+                        if (y>1)
+                        {
+                            y--;
+                            Console.Clear();
+                            Console.SetCursorPosition(x, y);
+                            Console.Write(smile);
+                            Console.SetCursorPosition(102, 27);
+                            Console.Write(x + "," + y);
+                            Console.SetCursorPosition(102, 5);
+                            Console.Write("Arriba ↑");
+                            Console.SetCursorPosition(x, y);
+
+                        }
+                        break;
+                        
+                        
+
+                }
+            }
+            while (tecla.Key != ConsoleKey.Spacebar);
+        }
+    }
+}
